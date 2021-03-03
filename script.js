b34610319cde61896fc3d30a512790c79478d4be
Richard Crippen <richard.crippen@gmail.com>
2021-03-03 12:31:44 -0500

    final code edits, add comments to all three files, delete console.logs from JS code.

--- script.js
+++ script.js
@@ -14,35 +14,40 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Begin password creation criteria
+//Begin password creation function
 
 function generatePassword() {
+//Empty string for creation of final password.
   var finalPass = "";
 
-  var passwordLength = parseInt(
-    prompt("Choose your password length between 8 and 25 characters.")
-  );
-  console.log(passwordLength);
+//Request numeric value from user and parse integer from user input.
+  var passwordLength = parseInt(prompt("Choose your password length between 8 and 25 characters."));
+
+//Check for numeric value of password and check to make sure the numeric value falls in
+//range of the specified requirement of at least 8 but no more than 25 characters
   if (passwordLength) {
-    if (passwordLength < 8 || passwordLength > 25) {
+    if (passwordLength < 8 || passwordLength > 25 || NaN) {
+//in the event the user selects an invalid number, user will receive an alert and have message
+//generate in the password box
       alert(
-        "Your password characters must be between 8 and 25 characters long."
-      );
+        "Your password characters must be between 8 and 25 characters long.");
       return "You enterd an invaild number of characters, please choose between 8 and 25 characters.";
     }
   }
+//4 confirm prompts for users to elect criteria that will be used to generate their password.
   var useUpperCase = confirm("Do you want to use upper case characters?");
-  console.log(useUpperCase);
   var useNumbers = confirm("Do you want to use numbers?");
-  console.log(useNumbers);
   var useLowerCase = confirm("Do you want to use lower case characters?");
-  console.log(useLowerCase);
   var useSpecial = confirm("Do you want to include special characters?");
-  console.log(useSpecial);
+//if statement to ensure that the user elects at least one criteria to create password
+//if user does not elect at least one criteria, then user will receive an alert and
+//message in password box to select at least one criteria for creation of password
   if (!useUpperCase && !useNumbers && !useLowerCase && !useSpecial) {
     alert("You must choose at least one of the criteria to create a password.");
-    return;
+    return "Please select 'OK' for at least one of the password criteria to have your password generated.";
   }
+//4 if statements looking for boolean values based on user input. If value
+//is true, then the variable string will be used to create the final password using the 'concat' method.
   if (useUpperCase === true) {
     finalPass = finalPass.concat(upperCase);
   }
@@ -55,10 +60,13 @@ function generatePassword() {
   if (useSpecial === true) {
     finalPass = finalPass.concat(special);
   }
-  var finalPass2 = "";
+//based on user data, a final variable is created using a for loop that uses the password length
+//to loop and retrieve the correct amount of values from the variable strings selected. A "randomPw" is created
+//using the math.floor function to round down the math.random * finalPass.length value.
+//finalPass2 is created and added to the finalPass and number value of the randomPw.
+  var finalPass2 = '';
   for (let i = 0; i < passwordLength; i++) {
     var randomPw = Math.floor(Math.random() * finalPass.length);
-    console.log(randomPw);
     finalPass2 = finalPass2.concat(finalPass[randomPw]);
   }
   return finalPass2;
